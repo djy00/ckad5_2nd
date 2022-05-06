52048f12e91297dda59aee44c6c15cc1933b68b0
djy00 <96768734+djy00@users.noreply.github.com>
2022-05-06 12:40:17 +0900

    Add files via upload

--- web/webapp.py
+++ web/webapp.py
@@ -14,6 +14,8 @@ import torch
 from flask import Flask, render_template, request, redirect,url_for
 import numpy as np
 
+
+# 폴더안의 파일 삭제 함수
 def DeleteAllFiles(filePath):
     if os.path.exists(filePath):
         for file in os.scandir(filePath):
@@ -24,9 +26,10 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        DeleteAllFiles('C:\\web\\yolov5-flask\\static\\aft')  #파일삭제
-        DeleteAllFiles('C:\\web\\yolov5-flask\\static\\bef')
+        DeleteAllFiles('C:\\web\\yolov5-flask\\static\\aft')  #파일 업로드 후 새로 검사 시작할 때마다 폴더 내 파일 삭제
+        DeleteAllFiles('C:\\web\\yolov5-flask\\static\\bef')  # 경로설정필요
         
+        # 다중파일 업로드
         if "file" not in request.files:
             return redirect(request.url)
         file = request.files.getlist("file")
@@ -35,7 +38,7 @@ def predict():
 
         pf=[]
         for file in file:
-            filename = file.filename.rsplit("/")[0]
+            filename = file.filename.rsplit("/")[0]     #파일경로에서 파일명만 추출
             print("진행 중 파일 :", filename)
 
             img_bytes = file.read()
@@ -44,9 +47,9 @@ def predict():
             img.save(f"static/bef/{filename}", format="JPEG")
             print('원본 저장')
 
-            results = model(img, size=640)
-            results.render()  # updates results.imgs with boxes and labels
-            data = results.pandas().xyxy[0][['name']].values.tolist()
+            results = model(img, size=1024)
+            results.render()  # results.imgs에 바운딩박스와 라벨 처리
+            data = results.pandas().xyxy[0][['name']].values.tolist()   # results.imgs의 name값만 가져오기
             print("데이터:",data)
 
             for img in results.imgs:
@@ -55,12 +58,12 @@ def predict():
                 print('디텍트 저장')
 
             if len(data) == 0:
-                pf.append("PASS")
+                pf.append("PASS")    # data 리스트의 값이 0이면 양품으로 pass
             if len(data) != 0:
-                pf.append("FAIL")
-            print(pf)
+                pf.append("FAIL")    # data 리스트의 값이 0이 아니면 불량으로 fail
+            
             root = "static/aft"
-            if not os.path.isdir(root):
+            if not os.path.isdir(root):      #파일명 리스트로 저장
                 return "Error : not found!"
             files = []
             for file in glob.glob("{}/*.*".format(root)):
@@ -68,7 +71,10 @@ def predict():
                 files.append(fname)
             print("파일스 :",files)
             firstimage = "static/aft/"+files[0]
+            
+            print(pf)
             print("파일1 :",firstimage)
+            
         return render_template("imageshow.html",files=files,pf=pf,firstimage=firstimage,enumerate=enumerate,len=len)
     return render_template("index.html")
 
@@ -91,97 +97,7 @@ if __name__ == "__main__":
     )
 
     app.run(**flask_options)
-    
-    
-    
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-#
-# @app.route("/upload", methods=["POST"])
-# def upload():
-#     """Handle the upload of a file."""
-#     form = request.form
-#     # Create a unique "session ID" for this particular batch of uploads.
-#     upload_key = str(uuid4())
-#
-#     # Is the upload using Ajax, or a direct POST by the form?
-#     is_ajax = False
-#     if form.get("__ajax", None) == "true":
-#         is_ajax = True
-#
-#     # Target folder for these uploads.
-#     target = "uploadr/static/uploads/{}".format(upload_key)
-#     try:
-#         os.mkdir(target)
-#     except:
-#         if is_ajax:
-#             return ajax_response(False, "Couldn't create upload directory: {}".format(target))
-#         else:
-#             return "Couldn't create upload directory: {}".format(target)
-#
-#     print("=== Form Data ===")
-#     for key, value in list(form.items()):
-#         print(key, "=>", value)
-#
-#     for upload in request.files.getlist("file"):
-#         filename = upload.filename.rsplit("/")[0]
-#         print("Accept incoming file:", filename)
-#
-#         destination = "/".join([target, filename])
-#         print("Save it to:", destination)
-#         upload.save(destination)
-#
-#     if is_ajax:
-#         return ajax_response(True, upload_key)
-#     else:
-#         return redirect(url_for("upload_complete", uuid=upload_key))
-#
-# @app.route("/files/<uuid>")
-# def upload_complete(uuid):
-#     """The location we send them to at the end of the upload."""
-#
-#     # Get their files.
-#     root = "uploadr/static/uploads/{}".format(uuid)
-#     if not os.path.isdir(root):
-#         return "Error: UUID not found!"
-#
-#     files = []
-#     for file in glob.glob("{}/*.*".format(root)):
-#         fname = file.split(os.sep)[-1]
-#         files.append(fname)
-#
-#     return render_template("files.html",
-#         uuid=uuid,
-#         files=files,
-#     )
-#
-#
-# def ajax_response(status, msg):
-#     status_code = "ok" if status else "error"
-#     return json.dumps(dict(
-#         status=status_code,
-#         msg=msg,
-#     ))
-#
 
-
-
-
-
-
-
-        # for debugging
-        # data = results.pandas().xyxy[0][['name']].values.tolist()
-        # data1 = results.pandas().xyxy[0][['confidence']].values.round(2).tolist()
-        #
-        # # return data
-        #
-        # results.render()  # updates results.imgs with boxes and labels
-        # for img in results.imgs:
-        #     img_base64 = Image.fromarray(img)
-        #     img_base64.save("static/image0.jpg", format="JPEG")
-        # # return redirect("static/image0.jpg")
         
 
 
